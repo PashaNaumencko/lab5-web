87620ae30b9822abc3e6743fb51a5945cf5b5d09
Pavlo Naumenko <p.naumenko@adbakers.com.ua>
2020-04-06 18:36:06 +0300

    Init project with basic enviroment configuration

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.[chunkhash].js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    port: 3000
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              configFile: "./babel.config.js",
+              cacheDirectory: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devtool: "source-map"
+};
