595303dbb60e5ba2eb507c389b64009f670efd5b
Pavlo Naumenko <p.naumenko@adbakers.com.ua>
2020-04-20 21:34:34 +0300

    Create recipe form with basic validation

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import 'bootstrap.native/dist/bootstrap-native-v4';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import RecipeList from './RecipeList';
+import RecipeForm from './RecipeForm';
 
 export default class App {
   constructor() {
@@ -15,8 +16,10 @@ export default class App {
 
   startApp() {
     const { element: recipes } = new RecipeList();
+    const { element: recipeForm } = new RecipeForm();
 
     recipesContainer.appendChild(recipes);
+    recipeFormContainer.appendChild(recipeForm);
   }
 
 }
---
+++ src/components/RecipeForm.js
@@ -0,0 +1,195 @@
+import Element from './Element';
+import { hasMinLength, isURL } from '../helpers/validationHelper';
+import { messages } from '../helpers/constants';
+
+export default class RecipeForm extends Element {
+  constructor() {
+    super();
+    this.errors = {
+      title: [],
+      imageUrl: [],
+      ingredients: [],
+      description: []
+    };
+
+    this.createForm();
+  }
+
+  createForm = () => {
+    this.element = this.createElement('form');
+    this.element.addEventListener('submit', this.onSubmit);
+
+    let inputs = [
+      this.wrapTo('col-sm-10', this.createTextInput({
+        name: 'title',
+        placeholder: 'Enter a recipe title',
+        id: 'title',
+        onBlur: this.validateTitle
+      })),
+      this.wrapTo('col-sm-10', this.createTextInput({
+        name: 'imageUrl',
+        placeholder: 'Enter a recipe image URL',
+        id: 'imageUrl',
+        onBlur: this.validateImageUrl
+      })),
+      this.wrapTo('col-sm-10', this.createTextArea({
+        name: 'ingredients',
+        placeholder: 'Enter a recipe ingredients',
+        id: 'ingredients',
+        onBlur: this.validateIngredients
+      })),
+      this.wrapTo('col-sm-10', this.createTextArea({
+        name: 'description',
+        placeholder: 'Enter a recipe description',
+        id: 'description',
+        onBlur: this.validateDescription
+      })),
+    ];
+
+    const labels = [
+      this.createLabel('title', 'Title'),
+      this.createLabel('imageUrl', 'Image URL'),
+      this.createLabel('ingredients', 'Ingredients'),
+      this.createLabel('description', 'Description')
+    ];
+
+    const submitButton = this.createSubmitButton('Submit');
+
+    inputs.forEach(input => {
+      const validFeedBack = this.createElement('div', { classNames: ['valid-feedback'] });
+      input.appendChild(validFeedBack);
+    });
+    const inputContainers = inputs.map((input, idx) => this.wrapTo('form-group', labels[idx], input));
+    this.element.append(...inputContainers, submitButton);
+  };
+
+  wrapTo = (className, ...elements) => {
+    const wrapper = this.createElement('div', { classNames: [className] });
+    wrapper.append(...elements);
+
+    return wrapper;
+  };
+
+  createLabel = (forAttr, text) => {
+    const label = this.createElement('label', {
+      classNames: ['col-sm-2', 'col-form-label'],
+      attributes: { for: forAttr }
+    });
+
+    label.innerText = text;
+    return label;
+  };
+
+  createTextInput = ({ name, placeholder, id, onBlur }) => {
+    const input = this.createElement('input', {
+      classNames: ['form-control'],
+      attributes: {
+        type: 'text',
+        name,
+        placeholder,
+        id
+      }
+    });
+
+    input.addEventListener('blur', onBlur);
+    return input;
+  };
+
+  createTextArea = ({ name, placeholder, id, onBlur }) => {
+    const textarea = this.createElement('textarea', {
+      classNames: ['form-control'],
+      attributes: {
+        name,
+        placeholder,
+        id
+      }
+    });
+
+    textarea.addEventListener('blur', onBlur);
+
+    return textarea;
+  };
+
+  createSubmitButton = (text) => {
+    const button = this.createElement('button', {
+      classNames: ['btn', 'btn-success', 'my-2', 'my-sm-0'],
+      attributes: {
+        type: 'submit'
+      }
+    });
+
+    button.innerText = text;
+    return button;
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const fields = event.target.elements;
+    if(this.validateForm(fields)) {
+      Array.from(fields).forEach(field => console.log(field.value));
+    }
+  };
+
+  validateForm = (fields) => [
+    this.validateTitle({ target: fields['title'] }),
+    this.validateImageUrl({ target: fields['imageUrl'] }),
+    this.validateIngredients({ target: fields['ingredients'] }),
+    this.validateDescription({ target: fields['description'] })
+  ].every(Boolean);
+
+  setValidationMessage = (field, messageElement, isValid = true) => {
+    field.classList.remove(isValid ? 'is-invalid' : 'is-valid');
+    field.classList.add(isValid ? 'is-valid' : 'is-invalid');
+    messageElement.classList.remove(isValid ? 'invalid-feedback' : 'valid-feedback');
+    messageElement.classList.add(isValid ? 'valid-feedback' : 'invalid-feedback');
+    messageElement.innerText = isValid ? messages.VALID : this.errors[field.name];
+  };
+
+  validateTitle = ({ target }) => {
+    const { value } = target;
+    if(!hasMinLength(value, 2)) {
+      this.errors['title'] = messages.MIN_VALUE;
+      this.setValidationMessage(target, target.nextSibling, false);
+      return false;
+    }
+    this.setValidationMessage(target, target.nextSibling);
+    return true;
+  };
+
+  validateImageUrl = ({ target }) => {
+    const { value } = target;
+    if(!hasMinLength(value, 2)) {
+      this.errors['imageUrl'] = messages.MIN_VALUE;
+      this.setValidationMessage(target, target.nextSibling, false);
+      return false;
+    } else if (!isURL(value)) {
+      this.errors['imageUrl'] = messages.INVALID_URL;
+      this.setValidationMessage(target, target.nextSibling, false);
+      return false;
+    }
+    this.setValidationMessage(target, target.nextSibling);
+    return true;
+  };
+
+  validateIngredients = ({ target }) => {
+    const { value } = target;
+    if(!hasMinLength(value, 2)) {
+      this.errors['ingredients'] = messages.MIN_VALUE;
+      this.setValidationMessage(target, target.nextSibling, false);
+      return false;
+    }
+    this.setValidationMessage(target, target.nextSibling);
+    return true;
+  };
+
+  validateDescription = ({ target }) => {
+    const { value } = target;
+    if(!hasMinLength(value, 2)) {
+      this.errors['description'] = messages.MIN_VALUE;
+      this.setValidationMessage(target, target.nextSibling, false);
+      return false;
+    }
+    this.setValidationMessage(target, target.nextSibling);
+    return true;
+  };
+}
