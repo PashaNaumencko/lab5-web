9f312e3e71e74eb2326d80c1775d0be18f5287e5
Pavlo Naumenko <p.naumenko@adbakers.com.ua>
2020-04-25 20:40:00 +0300

    Add navigation to app

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,34 @@ import RecipeForm from './RecipeForm';
 
 export default class App {
   constructor() {
-    this.startApp();
+    this.appContainer = document.getElementById('appContainer');
+    this.recipesListLink = document.getElementById('recipeListLink');
+    this.recipeFormLink = document.getElementById('recipeFormLink');
+
+    this.bindNavigationHandlers();
+    this.showPage(new RecipeList());
   }
 
-  static recipesContainer = document.getElementById('recipesContainer');
-  static recipeFormContainer = document.getElementById('recipeFormContainer');
+  bindNavigationHandlers = () => {
+    let previousTargetLink = this.recipesListLink;
+    this.recipesListLink.addEventListener('click', ({ target }) => {
+      previousTargetLink.parentNode.classList.remove('active');
+      target.parentNode.classList.add('active');
+      previousTargetLink = target;
+      this.showPage(new RecipeList());
+    });
+    this.recipeFormLink.addEventListener('click', ({ target }) => {
+      previousTargetLink.parentNode.classList.remove('active');
+      target.parentNode.classList.add('active');
+      previousTargetLink = target;
+      this.showPage(new RecipeForm());
+    });
+  };
 
-  startApp() {
-    const { element: recipes } = new RecipeList();
-    const { element: recipeForm } = new RecipeForm();
-
-    recipesContainer.appendChild(recipes);
-    recipeFormContainer.appendChild(recipeForm);
-  }
-
+  showPage = ({ element: page }) => {
+    while (this.appContainer.firstChild) {
+      this.appContainer.removeChild(this.appContainer.firstChild);
+    }
+    this.appContainer.appendChild(page);
+  };
 }
