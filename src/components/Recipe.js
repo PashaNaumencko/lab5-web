a15be00798b56c04c61ad7ae5e0042ec2596762b
Pavlo Naumenko <p.naumenko@adbakers.com.ua>
2020-04-25 23:27:00 +0300

    Add modal window with advanced recipe info

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,36 +1,42 @@
 import Element from './Element';
+import Modal from './Modal';
 
 export default class Recipe extends Element {
   constructor(recipe) {
     super();
 
+    this.recipe = recipe;
     this.createRecipeCard(recipe);
   }
 
-  createRecipeCard = ({ imgUrl, ...recipe }) => {
+  createRecipeCard = () => {
+    const { imgUrl } = this.recipe;
     this.element = this.createElement('div', { classNames: ['card', 'shadow'] });
     const image = this.createCardImage(imgUrl);
-    const body = this.createCardBody(recipe);
+    const body = this.createCardBody();
     this.element.append(image, body);
   };
 
-  createCardBody = ({ title, ingredients }) => {
-    const cardBody = this.createElement('div', { classNames: ['card-body'] });
+  createCardBody = () => {
+    const { title, ingredients } = this.recipe;
     const recipeTitle = this.createElement('h5');
     const recipeIngredients = this.createElement('p', { classNames: ['card-text', 'truncate-overflow'] });
     const button = this.createShowMoreButton('Show More');
 
     recipeTitle.innerText = title;
     recipeIngredients.innerText = ingredients;
-    cardBody.append(recipeTitle, recipeIngredients, button);
 
-    return cardBody;
+    return this.wrapTo('card-body', recipeTitle, recipeIngredients, button);
   };
 
   createShowMoreButton = (text) => {
     const showMoreButton = this.createElement('button', { classNames: ['btn', 'btn-success'] });
 
     showMoreButton.innerText = text;
+    showMoreButton.addEventListener('click', () => {
+      const { element: recipeModal} = new Modal(this.recipe);
+      document.getElementById('root').appendChild(recipeModal);
+    });
     return showMoreButton;
   };
 
