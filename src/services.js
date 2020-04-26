2d878a4104015abe82a879a22ed276cb7c6a202f
Pavlo Naumenko <p.naumenko@adbakers.com.ua>
2020-04-26 18:29:25 +0300

    Add API helper and services

--- src/services.js
+++ src/services.js
@@ -1,23 +1,29 @@
-export const getRecipeList = () => [
-  {
-    id: "1",
-    title: "Lasagna",
-    ingredients: "3/4 lb. lasagna noodles, 1 tsp. extra-virgin olive oil, plus more for drizzling, 2 lb. ground beef, 4 cloves garlic, 2 tsp. dried oregano, Kosher salt, Freshly ground black pepper, 2 (32-0z.) jars marinara, 16 oz. whole milk ricotta, 1/2 c. freshly grated Parmesan, 1/4 c. chopped parsley, 1 large egg, 2 lb. sliced mozzarella",
-    imgUrl: "https://us.123rf.com/450wm/jyliana/jyliana1710/jyliana171000041/88283504-traditional-italian-lasagna-with-minced-beef-bolognese-sauce-top-view.jpg?ver=6",
-    description: "Preheat oven to 375º. In a large pot of salted boiling water, cook pasta according to package directions until al dente, less 2 minutes. Drain and drizzle a bit of olive oil to prevent noodles from sticking together.<br><br>Meanwhile, in a large pot over medium-high heat, heat oil. Cook ground beef until no longer pink, breaking up with a wooden spoon. Remove from heat and drain fat. Return beef to skillet and add garlic and oregano and cook, stirring, for 1 minute. Season with salt and pepper, then add marinara and stir until warmed through. <br><br>Combine ricotta, 1/4 cup Parmesan, parsley, and egg in a large mixing bowl and season with salt and pepper. Set aside.<br><br>In a large casserole dish, evenly spread a quarter of the meat sauce across the bottom of the dish, then top with a single layer of lasagna noodles, a layer of ricotta mixture, and a single layer of mozzarella. Repeat layers, topping the last layer of noodles with meat sauce, Parmesan, and mozzarella.<br><br>Cover with foil and bake for 15 minutes, then increase temperature to 400º and bake uncovered for 18 to 20 minutes.<br><br>Garnish with parsley before serving."
-  },
-  {
-    id: "2",
-    title: "Spaghetti bolognese",
-    ingredients: "2 tbsp olive oil, 400g/14oz beef mince, 1 onion, 2 garlic cloves, 100g/3½oz carrot, 2 x 400g tin chopped tomatoes, 400ml/14fl oz stock, 400g/14oz dried spaghetti, salt and pepper",
-    imgUrl: "https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/easy_spaghetti_bolognese_93639_16x9.jpg",
-    description: "Heat a large saucepan over a medium heat. Add a tablespoon of olive oil and once hot add the beef mince and a pinch of salt and pepper. Cook the mince until well browned over a medium-high heat (be careful not to burn the mince. It just needs to be a dark brown colour). Once browned, transfer the mince to a bowl and set aside.<br><br>Add another tablespoon of oil to the saucepan you browned the mince in and turn the heat to medium. Add the onions and a pinch of salt and fry gently for 5-6 minutes, or until softened and translucent. Add the garlic and cook for another 2 minutes. Add the grated carrot then pour the mince and any juices in the bowl back into the saucepan.<br><br>Add the tomatoes to the pan and stir well to mix. Pour in the stock, bring to a simmer and then reduce the temperature to simmer gently for 45 minutes, or until the sauce is thick and rich. Taste and adjust the seasoning as necessary.<br><br>When ready to cook the spaghetti, heat a large saucepan of water and add a pinch of salt. Cook according to the packet instructions. Once the spaghetti is cooked through, drain and add to the pan with the bolognese sauce. Mix well and serve."
-  },
-  {
-    id: "3",
-    title: "Pizza Peperoni",
-    ingredients: "1 cup tomato-and-basil pasta sauce, 1 (10-oz.) package prebaked whole wheat thin Italian pizza crust, 1/4 cup turkey pepperoni slices, 1 1/2 cups (6 oz.) part-skim mozzarella cheese",
-    imgUrl: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fsl%2F03142008%2Fpepperoni-pizza-sl-1599569-x.jpg%3Fitok%3Dga1g5wpq",
-    description: "Spoon tomato-and-basil pasta sauce evenly over crust, leaving a 1-inch border around edges. Top with half of pepperoni slices. Sprinkle with cheese. Top with remaining pepperoni.<br><br>Bake pizza at 450° directly on oven rack 11 to 12 minutes or until crust is golden and cheese is melted. Cut into 6 slices. Serve immediately.<br><br>Note: For testing purposes only, we used Boboli 100% Whole Wheat Thin Pizza Crust and Classico Tomato & Basil Pasta Sauce."
+import { callApi } from './helpers/webApiHelper';
+
+export const getRecipes = async () => {
+  try {
+    const response = await callApi('/recipes.json', {
+      type: 'GET'
+    });
+
+    return Object.keys(response).map(id => ({
+      id,
+      ...response[id]
+    }));
+  } catch (error) {
+    console.log(error);
   }
-];
+}
+
+export const createRecipe = async (recipe) => {
+  try {
+    const response = await callApi('/recipes.json', {
+      type: 'POST',
+      body: recipe
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
